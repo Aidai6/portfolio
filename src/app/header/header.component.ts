3ab3b50010d78ef0a330fba9a7fd034f9d1da944
Aidai Almaz kyzy <aalmazkyzy@gmail.com>
2022-06-24 17:53:14 +0600

    completed header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 
+
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+
   faLinkedIn = faLinkedinIn;
   gitHub = faGithub;
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
